b0c285bee928e11923a9840a451d22302970c295
Betül Gök <se.betulgok@gmail.com>
2021-10-19 00:03:16 +0300

    18 Ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,28 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+console.log(euroDun)
+
+//array
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
